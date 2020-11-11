08075d35c4de9fac94aeb71a3bc5efc56e7c7757
oatter1234 <rachawas.092542@mail.kmutt.ac.th>
2020-11-12 00:29:10 +0700

    12/11 part 1

--- target/generated-sources/annotations/com/mycompany/webproject/entity/Admin_.java
+++ target/generated-sources/annotations/com/mycompany/webproject/entity/Admin_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-11T21:51:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-12T00:15:33")
 @StaticMetamodel(Admin.class)
 public class Admin_ { 
 
--- target/generated-sources/annotations/com/mycompany/webproject/entity/Customers_.java
+++ target/generated-sources/annotations/com/mycompany/webproject/entity/Customers_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-11T21:51:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-11-12T00:15:33")
 @StaticMetamodel(Customers.class)
 public class Customers_ { 
 
